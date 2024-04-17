4f20bafcf2f3c7e4dc4442d35a621efa6bc4d658
Олег Грачев <oagrachev@sberbank.ru>
2024-04-17 11:56:03 +0300

    import custom ui-kit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { MyButton, MyInput } from "first-ui-kit";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      WOW
+      <MyButton color="green">wow</MyButton>
+      <MyInput placeholder="lol" label="lol" isBig={true} />
     </div>
   );
 }
